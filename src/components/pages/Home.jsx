7fcc22ea86b2fb9319ac071f001f00db15b76472
H D U Chathushka <hdchathushka@gmail.com>
2025-07-05 20:18:23 +0530

    developing the header

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,13 @@
+import backgroundimg from '../images/backgroundimg.png';
 
 function Home() {
   return (
-    <div>
-      <div>
-        
+    <div className="relative w-full aspect-[3/2] overflow-hidden flex px-[60px]">
+      <img src={backgroundimg} alt="" className="mx-auto flex object-contain max-h-[75%] min-h-[150px] z-10"></img>
+      <div className="flex-col pt-8 uppercase font-extrabold lg:text-9xl absolute w-full h-full flex items-start">
+        <span>Welcome to the</span>
+        <span>world</span>
+        <span>of scent</span>
       </div>
     </div>
   );
