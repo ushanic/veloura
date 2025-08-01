e9981003dc56da29ddebfbf25af0faf95db79da5
H D U Chathushka <hdchathushka@gmail.com>
2025-08-01 15:14:26 +0530

    developed the NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import NavBar from "./components/Navbar";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Fragrances from "./components/pages/Fragrances";
+import Home from "./components/pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="m-[12px] border-2 border-black min-h-screen">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Fragrances" element={<Fragrances />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/pages/Fragrances.jsx
@@ -0,0 +1,9 @@
+function Fragrances() {
+  return (
+    <div>
+      <h1>Fragrances</h1>
+    </div>
+  );
+}
+
+export default Fragrances;
