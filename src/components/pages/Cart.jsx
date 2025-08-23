91b794510e3df52685863117fd9ae4de99e96ff7
H D U Chathushka <hdchathushka@gmail.com>
2025-08-23 15:55:00 +0530

    add the product and developed the quantity button

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import perfume1 from "../images/perfumes/dior/men/Sauvage_EauDeParfum.jpg";
 function Cart() {
+  const [quantity, setQuantity] = useState(1);
+  const decreaseQuantity = () => {
+    if (quantity > 1) setQuantity(quantity - 1);
+  };
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
   return (
     <div className="flex flex-col w-full pt-10 px-[60px]">
       <div className="flex flex-col max-w-[210px]">
@@ -13,23 +24,64 @@ function Cart() {
         <table className="w-full text-md text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-md text-gray-700 uppercase bg-gray-50 dark:bg-black dark:text-white">
             <tr>
-              <th scope="col" class="px-16 py-3">
-                <span class="sr-only">Image</span>
+              <th scope="col" className="px-16 py-3">
+                <span className="sr-only">Image</span>
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Product
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 quantity
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Price
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Action
               </th>
             </tr>
           </thead>
+
+          <tbody>
+            <tr className="border-b-2 border-black">
+              <td className="p-4">
+                <img
+                  src={perfume1}
+                  className="border-2 border-black w-12 h-12 md:h-24 md:w-24 max-w-full max-h-full"
+                  alt="perfume1"
+                ></img>
+              </td>
+              <td className="px-6 py-4 font-bold text-black text-md">
+                <div className="text-sm md:text-xl whitespace-nowrap">
+                  Sauvage Eau de Parfum
+                </div>
+                <div className="text-sm md:text-md whitespace-nowrap">
+                  Item No: 276453
+                </div>
+                <div className="text-sm md:text-md whitespace-nowrap">
+                  Size: 50ml
+                </div>
+              </td>
+
+              <td className="px-6 py-4">
+                <div className="grid grid-cols-3 items-center w-24 border-2 border-black text-black font-bold">
+                  <button
+                    className="border-r border-black text-center py-1"
+                    onClick={decreaseQuantity}
+                  >
+                    -
+                  </button>
+                  <div className="text-center">{quantity}</div>
+                  <button
+                    className="border-l border-black text-center py-1"
+                    onClick={increaseQuantity}
+                  >
+                    +
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
