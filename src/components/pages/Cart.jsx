188fc37ba80bfedab17ebc3d9828afe9512189dc
H D U Chathushka <hdchathushka@gmail.com>
2025-08-23 16:09:53 +0530

    completed the cart page

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa6";
 import perfume1 from "../images/perfumes/dior/men/Sauvage_EauDeParfum.jpg";
+
 function Cart() {
   const [quantity, setQuantity] = useState(1);
   const decreaseQuantity = () => {
@@ -80,9 +83,139 @@ function Cart() {
                   </button>
                 </div>
               </td>
+              <td className="px-6 py-4 font-bold text-black text-lg">$599</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-bold text-lg text-red-600 dark:text-red-500 hover:underline"
+                >
+                  Remove
+                </a>
+              </td>
+            </tr>
+
+            <tr className="border-b-2 border-black">
+              <td className="p-4">
+                <img
+                  src={perfume1}
+                  className="border-2 border-black w-12 h-12 md:h-24 md:w-24 max-w-full max-h-full"
+                  alt="perfume1"
+                ></img>
+              </td>
+              <td className="px-6 py-4 font-bold text-black text-md">
+                <div className="text-sm md:text-xl whitespace-nowrap">
+                  Sauvage Eau de Parfum
+                </div>
+                <div className="text-sm md:text-md whitespace-nowrap">
+                  Item No: 276453
+                </div>
+                <div className="text-sm md:text-md whitespace-nowrap">
+                  Size: 50ml
+                </div>
+              </td>
+
+              <td className="px-6 py-4">
+                <div className="grid grid-cols-3 items-center w-24 border-2 border-black text-black font-bold">
+                  <button
+                    className="border-r border-black text-center py-1"
+                    onClick={decreaseQuantity}
+                  >
+                    -
+                  </button>
+                  <div className="text-center">{quantity}</div>
+                  <button
+                    className="border-l border-black text-center py-1"
+                    onClick={increaseQuantity}
+                  >
+                    +
+                  </button>
+                </div>
+              </td>
+              <td className="px-6 py-4 font-bold text-black text-lg">$599</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-bold text-lg text-red-600 dark:text-red-500 hover:underline"
+                >
+                  Remove
+                </a>
+              </td>
+            </tr>
+
+            <tr className="border-b-2 border-black">
+              <td className="p-4">
+                <img
+                  src={perfume1}
+                  className="border-2 border-black w-12 h-12 md:h-24 md:w-24 max-w-full max-h-full"
+                  alt="perfume1"
+                ></img>
+              </td>
+              <td className="px-6 py-4 font-bold text-black text-md">
+                <div className="text-sm md:text-xl whitespace-nowrap">
+                  Sauvage Eau de Parfum
+                </div>
+                <div className="text-sm md:text-md whitespace-nowrap">
+                  Item No: 276453
+                </div>
+                <div className="text-sm md:text-md whitespace-nowrap">
+                  Size: 50ml
+                </div>
+              </td>
+
+              <td className="px-6 py-4">
+                <div className="grid grid-cols-3 items-center w-24 border-2 border-black text-black font-bold">
+                  <button
+                    className="border-r border-black text-center py-1"
+                    onClick={decreaseQuantity}
+                  >
+                    -
+                  </button>
+                  <div className="text-center">{quantity}</div>
+                  <button
+                    className="border-l border-black text-center py-1"
+                    onClick={increaseQuantity}
+                  >
+                    +
+                  </button>
+                </div>
+              </td>
+              <td className="px-6 py-4 font-bold text-black text-lg">$599</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-bold text-lg text-red-600 dark:text-red-500 hover:underline"
+                >
+                  Remove
+                </a>
+              </td>
             </tr>
           </tbody>
         </table>
+      </div>
+      <div className="mt-10 flex justify-end">
+        <h1 className="font-bold text-sm text-right">
+          shipping, taxes and discount calculated at checkout.
+        </h1>
+      </div>
+
+      <div className="pt-5 sm:pt-16 md:pt-10 flex justify-between items-center w-full">
+        {/* Continue Shopping - left */}
+        <button
+          type="button"
+          className="flex items-center gap-2 rounded-lg px-3 py-1.5 text-sm sm:px-4 sm:py-2 sm:text-base md:px-5 md:py-2.5 md:text-lg font-medium uppercase text-white dark:bg-black"
+        >
+          <FaArrowLeft className="text-red-600 text-base sm:text-lg md:text-xl" />
+          <span>continue shopping</span>
+        </button>
+
+        {/* Checkout - right */}
+        <button
+          type="button"
+          className="flex items-center gap-2 rounded-lg px-3 py-1.5 text-sm sm:px-4 sm:py-2 sm:text-base md:px-5 md:py-2.5 md:text-lg font-medium uppercase text-white dark:bg-black"
+        >
+          <span>checkout</span>
+          <FaArrowRight className="text-red-600 text-base sm:text-lg md:text-xl" />
+        </button>
       </div>
     </div>
   );
